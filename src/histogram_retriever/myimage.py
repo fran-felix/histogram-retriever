97fe4e25544bb90da99f8184447a11d17ccc2da9
fran-felix <franastico@gmail.com>
2026-08-19 10:07:00 -0400

    Adds make_histogram() method, loops not working properly.

--- src/histogram_retriever/myimage.py
+++ src/histogram_retriever/myimage.py
@@ -6,12 +6,30 @@ class MyImage:
                path="/home/francisco/histogram-retriever/input/input.png") -> None:
     self.name = name
     self.path = path  # path to input folder with the target image
-    self.img = Image.open(self.path)
-    self.img = np.array(self.img)
+    self.img = np.array(Image.open(self.path))
+    self.histogram = np.zeros((256, 3))
+    self.hist_alt = np.zeros((256, 3), dtype= int)
 
   def test_print(self):
     print(self.img)
-  
+
+  def make_histogram(self):
+    print(np.shape(self.img))
+    print(np.shape(self.histogram))
+
+    for i in range(self.img[:,:,0].size):
+      self.histogram[self.img.flat[i]][0] += 1
+    for i in range(self.img[:,:,1].size):
+      self.histogram[self.img.flat[i]][1] += 1
+    for i in range(self.img[:,:,2].size):
+      self.histogram[self.img.flat[i]][2] += 1
+    
+    # for i in np.nditer(self.img[:,:,2]):
+    #   self.histogram[i][2] += 1
 
   
-    
+img1 = MyImage()
+img1.make_histogram()
+
+print(img1.img[:,:,0])
+print(img1.histogram[:,0])
